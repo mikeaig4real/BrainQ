914085ec99a003f87721fd6f20ff9f9d99c2759b
mikeaig4real <mikeaig4realonreal@gmail.com>
2024-12-17 17:08:02 +0100

    added try/catch to decryption

--- utils/encryption.ts
+++ utils/encryption.ts
@@ -6,8 +6,8 @@ interface UserInfo {
 
 // Function to derive a stronger encryption key using PBKDF2 and email as salt
 export const deriveKey = (userInfo: UserInfo): string => {
-  const salt = CryptoJS.SHA256(userInfo.email).toString(); // Hash email to get a salt
-  const iterations = 100000; // Adjust iterations for better security
+  const salt: string = CryptoJS.SHA256(userInfo.email).toString(); // Hash email to get a salt
+  const iterations: number = 100000; // Adjust iterations for better security
   return CryptoJS.PBKDF2(JSON.stringify(userInfo), salt, { keySize: 256 / 32, iterations }).toString();
 };
 
@@ -18,6 +18,12 @@ export const encryptData = (data: any, key: string): string => {
 
 // Decrypt function: derives the key from user info (email as salt), then decrypts data
 export const decryptData = (encryptedData: string, key: string): any => {
-  const bytes = CryptoJS.AES.decrypt(encryptedData, key);
+  try
+  {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, key);
   return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  } catch ( e )
+  {
+    return null;
+  }
 };
